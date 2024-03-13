94c1c84c26efdc2bb8ed2713d17c927a7e0b2780
Leonardo Videc <lvidec5@gmail.com>
2024-03-13 16:51:58 +0100

    fix: label selection when on different page number

--- src/components/selection/LabelSelect.tsx
+++ src/components/selection/LabelSelect.tsx
@@ -4,6 +4,7 @@ import { Badge } from "@/components/ui/badge";
 import { useLabelsStore } from "@/lib/store/labelsStore";
 import { LabelWithIsActive } from "@/lib/types";
 import { cn } from "@/lib/utils";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useState } from "react";
 
 interface ILabelSelectProps {
@@ -12,6 +13,10 @@ interface ILabelSelectProps {
 }
 
 export function LabelSelect({ label, setFalsyOtherLabels }: ILabelSelectProps) {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const pathname = usePathname();
+
   const [isActive, setIsActive] = useState(false);
 
   const handleLabelSelection = useLabelsStore(
@@ -27,11 +32,21 @@ export function LabelSelect({ label, setFalsyOtherLabels }: ILabelSelectProps) {
     setFalsyOtherLabels(labelId);
   };
 
+  const replacePageParam = () => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", "1");
+
+    replace(`${pathname}?${params.toString()}`);
+  };
+
   return (
     <Badge
       variant={"outline"}
       className={cn("py-3 px-5", { "border-2 border-sky-700": label.isActive })}
-      onClick={() => handleLabelSelect(label.id)}
+      onClick={() => {
+        replacePageParam()
+        handleLabelSelect(label.id);
+      }}
     >
       {label.name}
     </Badge>
